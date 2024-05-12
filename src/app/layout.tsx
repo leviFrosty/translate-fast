a90d40964c7000cd8b11528e292332f2f3a8f20c
Levi Wilkerson <chelleas123@gmail.com>
2024-05-12 01:45:56 -0400

    feat: move to src directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-import "../env";
+import "../../env";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
